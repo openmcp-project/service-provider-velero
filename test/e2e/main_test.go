f62ba0446bfe839e2fa823a1523a09eff759e2d7
Christopher Junk <christopher.junk@sap.com>
2026-03-20 11:23:29 +0100

    fix: ocm helm (#13)
    * fix(task): ocm build
    
    On-behalf-of: @SAP christopher.junk@sap.com
    Signed-off-by: Christopher Junk <christopher.junk@sap.com>
    
    * feat: release v0.1.2
    
    On-behalf-of: @SAP christopher.junk@sap.com
    Signed-off-by: Christopher Junk <christopher.junk@sap.com>
    
    * fix(task): ocm build
    
    On-behalf-of: @SAP christopher.junk@sap.com
    Signed-off-by: Christopher Junk <christopher.junk@sap.com>
    
    ---------
    
    Signed-off-by: Christopher Junk <christopher.junk@sap.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -34,7 +34,7 @@ func TestMain(m *testing.M) {
 		ServiceProviders: []providers.ServiceProviderSetup{
 			{
 				Name:               "velero",
-				Image:              "ghcr.io/openmcp-project/images/service-provider-velero:v0.1.1",
+				Image:              "ghcr.io/openmcp-project/images/service-provider-velero:v0.1.2",
 				LoadImageToCluster: true,
 			},
 		},
