1889d39cad18998f704520d69f79ba882cb44194
René Schünemann <rene.schuenemann@sap.com>
2026-06-09 09:33:14 +0200

    chore: rename API group from velero.services.openmcp.cloud to velero.services.open-control-plane.io (#54)
    Rename the Kubernetes API group domain from openmcp.cloud to open-control-plane.io.
    Regenerated CRD manifests, RBAC roles, and deepcopy functions via 'task generate'.
    
    Part of https://github.com/openmcp-project/backlog/issues/533
    
    
    On-behalf-of: @SAP rene.schuenemann@sap.com
    
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the services v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=velero.services.openmcp.cloud
+// +groupName=velero.services.open-control-plane.io
 package v1alpha1
 
 import (
@@ -27,7 +27,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "velero.services.openmcp.cloud", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "velero.services.open-control-plane.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = runtime.NewSchemeBuilder(func(s *runtime.Scheme) error {
